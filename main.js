898c9ccd74e514eca756683aa4923538322ebef2
Pedro Anibarro <pedroanibarro3@gmail.com>
2018-02-01 15:30:01 -0400

    First commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow} = electron;
+
+let mainWindow;
+
+// Listen for app to be ready
+app.on('ready', function(){
+    
+    // Create new window
+    mainWindow = new BrowserWindow({
+        width: "1280",
+        height: "800",
+        center: true,
+        title: "Esmeraldina",
+        backgroundColor: "#000000"
+    });
+
+    // Load html into window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'resources/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+})
